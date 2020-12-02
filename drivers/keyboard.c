d07630beea5120049612c9f69816e7fa4c480634
ShintaroBRL <juliano0forum@gmail.com>
2020-12-01 21:06:57 -0300

    Splited keyboard.c with keymap.h

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -6,10 +6,7 @@
 #include "../libc/function.h"
 #include "../kernel/kernel.h"
 #include <stdint.h>
-
-#define BACKSPACE 0x0E
-#define CapsLock 0x3A
-#define ENTER 0x1C
+#include "keymap.h"
 
 static char key_buffer[256];
 static int CapsLockStatus = 0;
@@ -17,31 +14,6 @@ static int CapsLockStatus = 0;
 static int backspace_enabled_times = 0;
 
 #define SC_MAX 58
-/* UpperCase Chars */
-const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
-    "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
-        "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
-        "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
-        "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar", "CapsLock"};
-const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
-    '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
-        'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
-        'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V', 
-        'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' ', '?'};
-
-/* LowerCase Chars */
-const char *Lsc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
-    "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "q", "w", "e", 
-        "r", "t", "y", "u", "i", "o", "p", "[", "]", "Enter", "Lctrl", 
-        "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "`", 
-        "LShift", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar", "CapsLock"};
-const char Lsc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
-    '7', '8', '9', '0', '-', '=', '?', '?', 'q', 'w', 'e', 'r', 't', 'y', 
-        'u', 'i', 'o', 'p', '[', ']', '?', '?', 'a', 's', 'd', 'f', 'g', 
-        'h', 'j', 'k', 'l', ';', '\'', '`', '?', '\\', 'z', 'x', 'c', 'v', 
-        'b', 'n', 'm', ',', '.', '/', '?', '?', '?', ' ', '?'};
 
 static void keyboard_callback(registers_t *regs) {
     /* The PIC leaves us the scancode in port 0x60 */
@@ -59,7 +31,7 @@ static void keyboard_callback(registers_t *regs) {
         user_input(key_buffer); /* kernel-controlled function */
         key_buffer[0] = '\0';
         backspace_enabled_times = 0;
-    } else if (scancode == CapsLock) {
+    } else if (scancode == CAPSLUCK) {
         /* Toggle caps lock */
         if (CapsLockStatus)
             CapsLockStatus = 0;
