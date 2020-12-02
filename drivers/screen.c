a3f1d46f5b4780a541ad1d185f48530e2c98be8b
ShintaroBRL <juliano0forum@gmail.com>
2020-12-02 08:16:51 -0300

    screen color function

--- drivers/screen.c
+++ drivers/screen.c
@@ -3,9 +3,13 @@
 #include "../libc/mem.h"
 #include <stdint.h>
 
+
+static int color = WHITE_ON_BLACK;
+
 /* Declaration of private functions */
 int get_cursor_offset();
 void set_cursor_offset(int offset);
+void screen_color(int color_hex);
 int print_char(char c, int col, int row, char attr);
 int get_offset(int col, int row);
 int get_offset_row(int offset);
@@ -33,7 +37,7 @@ void kprint_at(char *message, int col, int row) {
     /* Loop through message and print it */
     int i = 0;
     while (message[i] != 0) {
-        offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
+        offset = print_char(message[i++], col, row, color);
         /* Compute row/col for next iteration */
         row = get_offset_row(offset);
         col = get_offset_col(offset);
@@ -48,7 +52,15 @@ void kprint_backspace() {
     int offset = get_cursor_offset()-2;
     int row = get_offset_row(offset);
     int col = get_offset_col(offset);
-    print_char(0x08, col, row, WHITE_ON_BLACK);
+    print_char(0x08, col, row, color);
+}
+
+/**
+ * change the text and backgound color
+ * use hex values like: 0x0f
+ */
+void screen_color(int color_hex){
+    color = color_hex;
 }
 
 
@@ -67,7 +79,7 @@ void kprint_backspace() {
  */
 int print_char(char c, int col, int row, char attr) {
     uint8_t *vidmem = (uint8_t*) VIDEO_ADDRESS;
-    if (!attr) attr = WHITE_ON_BLACK;
+    if (!attr) attr = color;
 
     /* Error control: print a red 'E' if the coords aren't right */
     if (col >= MAX_COLS || row >= MAX_ROWS) {
@@ -139,7 +151,7 @@ void clear_screen() {
 
     for (i = 0; i < screen_size; i++) {
         screen[i*2] = ' ';
-        screen[i*2+1] = WHITE_ON_BLACK;
+        screen[i*2+1] = color;
     }
     set_cursor_offset(get_offset(0, 0));
 }
